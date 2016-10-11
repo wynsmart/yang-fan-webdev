e9da2a1b9db418750ca0c8cce3a8b31d896fc59e
Fan Yang <wyns.smart@gmail.com>
2016-10-11 02:40:11 -0400

    tried: ng-route

--- public/assignment/js/app.js
+++ public/assignment/js/app.js
@@ -1,15 +1,19 @@
 (function () {
     angular
-        .module("WebAppMaker")
+        .module("WebAppMaker",['ngRoute'])
         .config(Config);
 
     function Config($routeProvider) {
         $routeProvider
             .when("/login", {
-                templateUrl: "/views/user/login.view.client.html"
+                templateUrl: "views/user/login.view.client.html"
+            })
+            .when("/register", {
+                templateUrl: "views/user/register.view.client.html"
             })
             .otherwise({
                 redirectTo: "/login"
             });
     }
+
 })();
