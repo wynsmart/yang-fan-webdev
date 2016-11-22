5565f3592299653b7732ffef8da7b91d75c30eaa
Fan Yang <wyns.smart@gmail.com>
2016-11-22 15:38:07 -0500

    continuous push

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
